3a3cb8c507a7b283bc79fde1517977031cc2a93e
Tristan Hisgen <tristan.hisgen@mni.thm.de>
2019-05-15 17:29:45 +0200

    add train station job and widget

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'smashing'
+gem 'activesupport'
+gem 'rest-client'
+
 
 ## Remove this if you don't need a twitter widget.
 gem 'twitter', '>= 5.9.0'
---
+++ jobs/next_train_connections.rb
@@ -0,0 +1,43 @@
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+require 'rest-client'
+require 'active_support/core_ext/hash/conversions'
+
+luebeckEva = "8000124"
+authKey = "x-x-x-x"
+
+def eventName(label, event)
+  if event['l']
+    "#{label['c']} #{event['l']}"
+  else
+    "#{label['c']} #{label['n']}"
+  end
+end
+
+SCHEDULER.every '300s', :first_in => 0 do |job|
+  puts 'next train connection'
+
+  date = Time.now.strftime("%y%m%d")
+  hour = Time.now.strftime("%H")
+  # TODO hour + 1 ist falsch fuer hour == 23!
+  nextHour = (hour.to_i + 1).to_s
+
+  url = "https://api.deutschebahn.com/timetables/v1/plan/#{luebeckEva}/#{date}/#{nextHour}"
+  response = RestClient.get(url, headers={"Authorization": "Bearer #{authKey}"})
+  data = Hash.from_xml(response.body)['timetable']['s']
+
+  # url = "https://api.deutschebahn.com/timetables/v1/plan/#{luebeckEva}/#{date}/#{nextHour}"
+  # response = RestClient.get(url, headers={"Authorization": "Bearer #{authKey}"})
+  # data += Hash.from_xml(response.body)['timetable']['s']
+  # TODO Verbindungen die um hour ankommen und um nextHour abfahren sind zweimal in data enthalten
+
+  abfahrten = data.select{ |s| s.has_key?('dp') }.sort_by{ |s| s['dp']['pt'] }
+  items = abfahrten.map do |s|
+    name = eventName(s['tl'], s['dp'])
+    time = Time.strptime(s['dp']['pt'], "%y%m%d%H%M")
+    stationen = s['dp']['ppth'].split('|')
+    {name: name, time: time, stationen: stationen, gleis: s['dp']['pp']}
+  end
+  items = items.select{ |item| item[:time] > Time.now }
+
+  send_event('nextTrainConnections', { items: items.map { |i| {label: "#{i[:name]} #{i[:stationen].last}", value: i[:time].strftime('%H:%M') }}})
+end
